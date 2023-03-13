590499e2cc65ec1b3d24e4fa079d1bc7303b1056
yuseob <dladbtjq@naver.com>
2023-03-13 13:01:33 +0900

    11

--- fulljoo/src/main/java/com/example/demo/FulljooApplication.java
+++ fulljoo/src/main/java/com/example/demo/FulljooApplication.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//으야야ㅑ캬캬ㅇㅁㅇㅁ
+//으야야ㅑ캬캬ㅇㅁㅇㅁㅇㅇㅇ
 @SpringBootApplication
 public class FulljooApplication {
 
